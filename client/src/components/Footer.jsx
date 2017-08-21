5b97df05baf5bef5224f4b0de08bf5ca3972dd57
Militsa Neykova <militsaneykovany@gmail.com>
2017-08-21 15:04:31 -0400

    add Header and Footer components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      Copyright@2017
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+        <div className="navWide">
+          <a href="#">Log in</a>
+          <a href="#">Register</a>
+          <a href="#">Log out</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Header;
