c6a4a1e59df3403cb59041684826066a96d69556
Mitchel Severe <truth531@gmail.com>
2017-08-21 16:32:33 -0400

    fixed component typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './common/Header';
-import Footer from './common/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 class App extends Component {
