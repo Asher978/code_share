a6449f7495d1a832cda119e2f6d5496dca9601e3
Militsa Neykova <militsaneykovany@gmail.com>
2017-08-21 16:51:02 -0400

    add LogIn Nav and Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './common/Header';
-import Footer from './common/Footer';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import EventList from './components/EventList';
+import ChallengesList from './components/ChallengesList';
+import Login from './components/Login';
+import Register from './components/Register';
+import SingleChallenge from './components/SingleChallenge';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -10,6 +20,12 @@ class App extends Component {
       <Router >
       <div className="App">
         <Header />
+        <Route exact path= "/" component={Home}/>
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Register" component={Register} />
+        <Route exact path="/ChallengesList" component={ChallengesList} />
+        <Route exact path="/EventList" component={EventList} />
+        <Route exact path="/SingleChallenge/:id" component={SingleChallenge} />
         <Footer />
       </div>
       </Router>
