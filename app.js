faf12e81d5d04dfe289ce95f88863dac29662211
Asher Shaheen <ashershaheen@gmail.com>
2017-08-21 15:01:07 -0400

    changed the port to 3000

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(passport.session());
 app.use(express.static('public'));
 
 //port setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Alive on port ${PORT}`);
 });
