8f412d7cdc876005275372f03e7630376ad69bdc
Mitchel Severe <truth531@gmail.com>
2017-08-21 17:50:29 -0400

    add state

--- client/src/components/SingleChallenge.jsx
+++ client/src/components/SingleChallenge.jsx
@@ -1,7 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Codemirror from 'react-codemirror';  
+import 'codemirror/lib/codemirror.css';  
+import 'codemirror/theme/monokai.css';  
+import 'codemirror/mode/javascript/javascript.js';
+
+class SingleChallenge extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      code: '',
+    }
+  }
+
+  handleUpdateCodeState = (code) => {
+    this.setState({
+      code: code,
+    });
+  }
+
+  render() {
+    const options = {
+      lineNumbers: true,
+      mode: 'javascript',
+      theme: 'monokai'
+    }
+    
+    return (
+      <Codemirror
+        value={this.state.code}
+        onChange={this.handleUpdateCodeState}
+        options={options} 
+      />
+    )
+  }
+}
 
 
 
-
-
-export default Login;
+export default SingleChallenge;
