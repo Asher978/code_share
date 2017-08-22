8e612deaa04760cfc690936047d8a025e3e0b0e8
Mitchel Severe <truth531@gmail.com>
2017-08-22 02:17:18 -0400

    add bootstrap jumbotron and corporate ipsum text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
 
 const Home = () => {
   return (
-    <div></div>
-  )
+    <div>
+      <Jumbotron className="main">
+        <div className="content">
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">Intrinsicly optimize extensive web services before empowered architectures..</p>
+          <hr className="my-2" />
+          <p>Rapidiously provide access to customer directed action items and accurate bandwidth.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </div>
+      </Jumbotron>
+    </div>
+  );
 }
 
 export default Home;
