fd02d78a7ec685eb9b1f385648577e4658239f20
Militsa Neykova <militsaneykovany@gmail.com>
2017-08-22 12:17:00 -0400

    'add auth to App.js'

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,41 @@ import Register from './components/Register';
 import SingleChallenge from './components/SingleChallenge';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import io from 'socket.io-client';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
-
+    this.state ={
+      auth: false,
+      user: null,
+      currentPage: 'home',
+      currentMovieId: null,
+    }
     this.state = {
       status: 'disconnected',
+    }
+    this.setPage = this.setPage.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+  }
+
+  setPage(page) {
+    console.log('click');
+    this.setState({
+      currentPage: page,
+    })
+  }
+  decideWhichPage() {
+    switch(this.state.currentPage) {
+      case 'home' :
+        return <Home />;
+      case 'login':
+        return <Login handleLoginSubmit={this.handleLoginSubmit} />
+      case 'register':
+        return <Register handleRegisterSubmit={this.handleRegisterSubmit} />
+      default:
+      break;
     }
   }
 
@@ -37,13 +65,40 @@ class App extends Component {
       status: 'disconnected',
     });
   }
+  handleLoginSubmit(e, username, password) {
+    e.preventDefault();
+    axios.post('/auth/login', {
+      username, 
+      password,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user
+      });
+    }).catch(err => console.log(err));
+  }
+  handleRegisterSubmit(e, username, password, email) {
+    e.preventDefault();
+    axios.post('/auth/register', {
+      username,
+      password,
+      email,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+        currentPage: 'home',
+      });
+    }).catch(err => console.log(err));
+  }
 
   render() {
     return (
       <Router >
       <div className="App">
         <div className="none">
-          <Header />
+          <Header setPage={this.setPage}/>
+          {this.decideWhichPage()}
           <Route exact path= "/" component={Home}/>
           <Route path="/challenges" component={Challenges} />
           <Route path="/events" component={Events} />
