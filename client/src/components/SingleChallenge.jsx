5f22528c1580a5e73c74b7cba5ec53cbed6c0c47
Mitchel Severe <truth531@gmail.com>
2017-08-23 10:08:56 -0400

    create message, messages state and add socket helper function

--- client/src/components/SingleChallenge.jsx
+++ client/src/components/SingleChallenge.jsx
@@ -15,16 +15,24 @@ class SingleChallenge extends Component {
     this.state = {
       code: '',
       status: 'disconnected',
+      message: '',
+      messages: [],
     }
   }
 
   componentDidMount() {
     this.socket.on('connect', this.connect);
+
     this.socket.emit('room', {
       room: this.props.match.params.single,
     });
+
     this.socket.on('coding', (data) => {   
       this.updateCodeFromSockets(data);
+    });
+
+    this.socket.on('chat messages', (data) => {
+      this.updateMsgFromSockets(data);
     });
   }
 
@@ -41,6 +49,15 @@ class SingleChallenge extends Component {
     });
   }
 
+  updateMsgFromSockets = (data) => {
+    const updatedMessages = [...this.state.messages];
+    updatedMessages.push(data);
+    this.setState({
+      messages: updatedMessages,
+    });
+    console.log('front-end message update', this.state.messages);
+  }
+
   handleUpdateCodeState = (text) => {
     this.setState({
       code: text,
@@ -49,6 +66,21 @@ class SingleChallenge extends Component {
       room: this.props.match.params.single,
       code: this.state.code, 
     });
+  }
+
+  handleSubmit = (e) => {
+    console.log('clicked');
+    e.preventDefault();
+    this.socket.emit('message chat', {
+      room: this.props.match.params.single,
+      message: this.state.message, 
+    });
+    this.setState({message: ''});
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({message: e.target.value});
   }
 
   connect = () => {
@@ -80,9 +112,14 @@ class SingleChallenge extends Component {
               onChange={this.handleUpdateCodeState}
               options={options}
             />
+            <p>Message: {this.state.message}</p>
           </Col>
           <Col md="2">
-            <Chat />
+            <Chat handleSubmit={this.handleSubmit} 
+                  value={this.state.message} 
+                  handleChange={this.handleChange}
+                  messages={this.state.messages}
+            />
           </Col>
         </Row>
       </Container>
