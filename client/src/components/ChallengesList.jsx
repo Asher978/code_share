54584d4b1edd2a58ed8110a993018c7bab3af676
Asher Shaheen <ashershaheen@gmail.com>
2017-08-24 19:48:59 -0400

    imported challenges and mapped them for rendering

--- client/src/components/ChallengesList.jsx
+++ client/src/components/ChallengesList.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import challenge from '../challenge/challenge';
+import { Link } from 'react-router-dom';
 
-const Challenges = () => {
-  return (
-    <div></div>
-  )
+class Challenges extends Component {
+  render () {
+    return (
+    <div>
+     {challenge.map(c => {
+       return (
+         <div id={c.id}>
+            <Link challenge={c} to={`/challenges/${c.id}`}>Challenge # {c.id}</Link>
+            <p>{c.chall}</p>
+         </div>
+       )
+     })}
+    </div>
+    )
+  }
+  
 }
 
 export default Challenges;
