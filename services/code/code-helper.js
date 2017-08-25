969736ba64accf93ac9bb062b7f4625a1592f9f9
Asher Shaheen <ashershaheen@gmail.com>
2017-08-25 10:46:29 -0400

    added check error package to work with sandbox and codeEval

--- services/code/code-helper.js
+++ services/code/code-helper.js
@@ -1,20 +1,20 @@
 const vm = require('vm');
 const assert = require('assert');
 const expect = require('chai').expect;
+const checkError = require('check-error');
 
 
 let codeEval = (req, res, next) => {
     let result;
     let code = req.body.code;
 
-    const sandbox = { assert: assert, expect: expect };
+    const sandbox = { assert: assert, expect: expect, checkError: checkError };
     vm.createContext(sandbox);
     try {
         result = vm.runInContext(code, sandbox);
         res.locals.ref = result;  
     } catch (e) {
-        console.log(e.message);
-        result = e.message;
+        result = checkError.getMessage(e);
         res.locals.ref = result;
     }
     next();
